dd3870f32b6dfa7d30e5bd84ed3e431559b874ef
speyyeti068 <75049648+speyyeti068@users.noreply.github.com>
2021-06-28 19:27:23 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
+var leftWristX = 0;
+var leftWristY = 0;
+
+var rightWristX = 0;
+var rightWristY = 0;
+
 function preload() {
     song = loadSound("music.mp3");
 }
@@ -8,6 +14,28 @@ function setup() {
 
     video = createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, function () {
+        console.log("Model Loaded");
+    });
+
+    poseNet.on("pose", gotPoses);
+}
+
+function gotPoses(results) {
+    if (results.length > 0) {
+        console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        leftWristScore = results[0].pose.keypoints[9].score;
+
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        rightWristScore = results[0].pose.keypoints[10].score;
+
+        console.log(leftWristX, leftWristY, rightWristX, rightWristY, leftWristScore, rightWristScore);
+    }
 }
 
 function draw() {
